87d05421ed0af80cec784bd71fbf18a11f5f4b02
unidos por antioquia <escanerunidos2026@gmail.com>
2026-07-29 13:42:29 -0500

    actualizar codigo

--- js/admin.js
+++ js/admin.js
@@ -1,15 +1,18 @@
-const URL_HOJA = "https://script.google.com/macros/s/AKfycbx_DKK2VWgyDqoN_a50nDU4_8PhWSTI-6A6-yyZ3-DoALTEAXiwli3hKMg-jHaXfzcaAg/exec";
+const URL_HOJA = "https://script.google.com/macros/s/AKfycbyvoGf2LBAtNsAzBjcBm5ziI1Hx7AjMMkLjOivyWVmCZg-ULW3OimZmZaqTyniWpjgunA/exec";
 
 async function cargarEstadisticas() {
+    try {
+        const respuesta = await fetch(URL_HOJA);
+        const datos = await respuesta.json();
 
-    const respuesta = await fetch(URL_HOJA);
-    const datos = await respuesta.json();
+        document.getElementById("consultas").textContent = datos.consultas;
+        document.getElementById("pdf").textContent = datos.pdfs;
+        document.getElementById("hoy").textContent = datos.visitantesHoy;
+        document.getElementById("mes").textContent = datos.visitantesMes;
 
-    document.getElementById("consultas").textContent = datos.consultas;
-    document.getElementById("pdf").textContent = datos.pdf;
-    document.getElementById("hoy").textContent = datos.hoy;
-    document.getElementById("mes").textContent = datos.mes;
-
+    } catch (error) {
+        console.error("Error al cargar estadísticas:", error);
+    }
 }
 
 cargarEstadisticas();
