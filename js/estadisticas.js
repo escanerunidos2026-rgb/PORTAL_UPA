23cec1be1210e25e3aa0812abf25e3fa43f1a791
unidos por antioquia <escanerunidos2026@gmail.com>
2026-08-03 10:04:49 -0500

    actualizar pagina

--- js/estadisticas.js
+++ js/estadisticas.js
@@ -1,4 +1,5 @@
 const URL_HOJA = "https://script.google.com/macros/s/AKfycby-_v7Fz0twg_lX6KsRzSSmNt_5W272mK5w9ZU8YtiLmH9AsICk9HRt7iUpKgbSfgikvw/exec";
+
 let visitante = localStorage.getItem("visitanteUPA");
 
 if (!visitante) {
@@ -6,5 +7,14 @@ if (!visitante) {
     localStorage.setItem("visitanteUPA", visitante);
 }
 
-fetch(URL_ESTADISTICAS + "?accion=Visita&id=" + visitante)
-    .catch(() => {});
+const hoy = new Date().toLocaleDateString();
+const ultimaVisita = localStorage.getItem("ultimaVisitaUPA");
+
+if (ultimaVisita !== hoy) {
+
+    localStorage.setItem("ultimaVisitaUPA", hoy);
+
+    fetch(URL_HOJA + "?accion=Visita&id=" + visitante)
+        .catch(() => {});
+
+}
